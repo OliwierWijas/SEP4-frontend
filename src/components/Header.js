2bffb1e2bf2a2fccad20ad33fbd17d6350197e5c
Oliwier <oliwier.wijas@interia.pl>
2024-05-09 08:23:56 +0200

    Setting footer to always generate in the bottom of the page

--- src/components/Header.js
+++ src/components/Header.js
@@ -95,7 +95,6 @@ export default function Header() {
         )}
       </nav>
       <BrownBreakline />
-      <Outlet />
     </>
   );
 }
--- src/routes/App.js
+++ src/routes/App.js
@@ -1,13 +1,15 @@
 import "../index.css";
 import Header from "../components/Header.js";
 import Footer from "../components/Footer.js";
+import { Outlet } from "react-router-dom";
 
 export default function App() {
   return (
     <>
-      <div className="w-4/5 mx-auto">
-        <Header></Header>
-        <Footer></Footer>
+      <div className="w-4/5 mx-auto min-h-screen flex flex-col">
+        <div className="justify-start"><Header></Header></div>
+        <div className="flex-1"><Outlet /></div>
+        <div className="justify-end"><Footer></Footer></div>
       </div>
     </>
   );
