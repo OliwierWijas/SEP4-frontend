5fa9ebfd5adef3aba7dfcc25b1ad6145355b43d6
Maria B <331289@viauc.dk>
2024-05-18 16:04:19 +0200

    Bit cleaning and started component tree

--- src/components/MyHome/GraphComponent/GraphComponent.js
+++ src/components/MyHome/GraphComponent/GraphComponent.js
@@ -1,5 +1,5 @@
 import { Line } from 'react-chartjs-2';
-import {Chart as ChartJS} from "chart.js/auto"
+
 
 function GraphComponent({ data }) {
   return <Line data={data} height={200}/>
--- src/routes/MyProfile.js
+++ src/routes/MyProfile.js
@@ -1,7 +1,4 @@
-import { useState } from "react";
 import EditDeleteAccount from "../components/MyProfile/EditDeleteAccount.js";
-import PopUp from "../components/PopUp.js";
-import ConfirmDelete from "../components/MyProfile/ConfirmWithPassword.js";
 import HouseMembersBoxComponent from "../components/MyProfile/HouseMembersBoxComponent.js";
 import EditLockPassword from "../components/MyProfile/EditLockPasswordComponent.js";
 
