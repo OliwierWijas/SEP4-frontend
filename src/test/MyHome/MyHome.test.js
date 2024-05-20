4aa65bf54f12b052624f72924eb651345ce3fadb
Oliwier <oliwier.wijas@interia.pl>
2024-05-20 10:38:21 +0200

    Adapting testing to new environment

--- src/test/MyHome/MyHome.test.js
+++ src/test/MyHome/MyHome.test.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { render, screen, fireEvent, waitFor } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect'
 import MyHome from '../../routes/MyHome.js'
 import { Chart } from 'chart.js';
